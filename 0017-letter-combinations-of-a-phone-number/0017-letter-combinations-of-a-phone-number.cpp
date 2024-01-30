db4d0700f4acb09db4bc037155003f390645c16f
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2024-01-30 18:34:24 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (13.36%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void Fun(int i,int j,string &digits,char *ans,vector<string> &res,char keypad[][10]){
+        if(digits[i] == '\0'){
+            if(ans[0] != '\0')
+            res.push_back(ans);
+            return ;
+            }
+        int digit = digits[i]-'0';
+        if(digit == 0 or digit == 1)
+        {
+            Fun(i+1,j,digits,ans,res,keypad);
+        }
+        for(int k = 0; k < keypad[digit][k] != '\0';k++){
+            ans[j] = keypad[digit][k];
+            Fun(i+1,j+1,digits,ans,res,keypad);
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string>res;
+        char keypad[][10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        char ans[100];
+        Fun(0,0,digits,ans,res,keypad);
+        if(digits.size() == 0) return {};
+        return res;
+    }
+};
